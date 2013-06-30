ab9879933cb9fba158b5005c6a91f985f45af7ef
Adam Wright <adam@hipikat.org>
2013-06-30 20:12:46 +0800

    PEP8 compliance.

--- src/hipikat/settings/base.py
+++ src/hipikat/settings/base.py
@@ -3,6 +3,9 @@
 from revkom.settings import settings_path
 
 
+g = globals()
+
+
 ###
 # Local project settings
 ###
@@ -23,30 +26,30 @@ CACHES = {'default': {
 
 ###
 # Inherit from a fine set of default settings. (Note: this also sets g and s.)
-DEBUG = False if 'DEBUG' not in globals() else DEBUG
+DEBUG = globals().get('DEBUG', False)
 execfile(settings_path.child('base_prod.py' if not DEBUG else 'base_debug.py'))
 
 ###
 # Django
 ###
 # Install apps after the main application.
-INSTALLED_APPS = INSTALLED_APPS[0:1] + [
+INSTALLED_APPS = g['INSTALLED_APPS'][0:1] + [
     # django-crispy-forms: Forms have never been this crispy.
     # http://django-crispy-forms.readthedocs.org/en/latest/
     'crispy_forms',
 
     # django-fluent-blogs: A basic blogging engine.
     # https://github.com/edoburu/django-fluent-blogs/
-    'fluent_blogs',
+    #'fluent_blogs',
 
     # django-fluent-contents: The fluent_contents module offers a widget.
     # engine to display various content on a Django page.
     # https://github.com/edoburu/django-fluent-contents
-    'fluent_contents',
-    'fluent_contents.plugins.text',
+    #'fluent_contents',
+    #'fluent_contents.plugins.text',
 
     # django-wysiwyg: Converts HTML textareas into rich HTML editors.
     # https://github.com/pydanny/django-wysiwyg
-    'django_wysiwyg',
+    #'django_wysiwyg',
 
-] + INSTALLED_APPS[1:]
+] + g['INSTALLED_APPS'][1:]
