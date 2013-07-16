513fa281299dac68308b59f59e476b9900b00582
Adam Wright <adam@hipikat.org>
2013-07-16 12:45:39 +0800

    Fresh Foundation theme dev setup

---
+++ src/hipikat/middleware.py
@@ -0,0 +1,8 @@
+
+class DebugOuterMiddleware(object):
+    def process_request(self, request):
+        pass
+
+class DebugInnerMiddleware(object):
+    def process_request(self, request):
+        pass
