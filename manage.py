38abdb2114ad883129de545d13c882db6256aaec
Adam Wright <adam@hipikat.org>
2013-10-22 18:53:15 +0800

    Bug zapping, more docs

--- manage.py
+++ manage.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python
-from os import environ
-from os.path import dirname, join
-from sys import argv
+import os
+from os import path
+import sys
 import envdir
 
 if __name__ == "__main__":
-    # Copy text from files in var/env/ into environment variables
-    envdir.read(join(dirname(__file__), 'var/env'))
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hipikat.settings")
 
-    # Default to a dumb configuration, which provides its own (insecure)
-    # SECRET_KEY and uses var/db/default.db for its (Sqlite) database.
-    environ.setdefault("DJANGO_SETTINGS_MODULE", "hipikat.settings")
-    if "DJANGO_CONFIGURATION" not in environ or 'make_secret_key' in argv:
-        environ["DJANGO_CONFIGURATION"] = "Default"
+    # TODO: Read this stuff into the environment upon activation of the
+    # virtual environment, instead.
+    envdir.read(path.join(path.dirname(__file__), 'var/env'))
 
-    # Load the django-configurations importer and run the command line
-    from configurations.management import execute_from_command_line
-    execute_from_command_line(argv)
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
