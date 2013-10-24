e030b7702efb9176254f50c71154f4c9f89e05cb
Adam Wright <adam@hipikat.org>
2013-10-24 14:13:28 +0800

    Fix (and add template for) debug scratch view

--- src/hipikat/views/debug.py
+++ src/hipikat/views/debug.py
@@ -2,7 +2,11 @@
 from django.shortcuts import render
 
 
+def scratch(request):
+    return render(request, "debug/scratch.html")
+
+
 def blog_layout(request):
-        return render(request, "dev/blog-layout.html")
+    return render(request, "dev/blog-layout.html")
 
 
