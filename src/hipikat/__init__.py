c93a4837f90666c62b3edec1d37bbaaabc41d553
Adam Wright <adam@hipikat.org>
2013-10-29 09:03:51 +0800

    Add settings context processor, debug toolbar toggle, various fixes
    - Project-level context_processor; copies selected settings to context
    - Add toggle for django-debug-toolbar
    - Fix typos, unclosed blocks in settings and base template

--- src/hipikat/__init__.py
+++ src/hipikat/__init__.py
@@ -1,1 +1,19 @@
 # hipikat/__init__.py
+
+from django.conf import settings
+
+
+CONTEXT_SETTINGS_VARIABLES = (
+    'PROJECT_MODULE',
+)
+
+def context_processor(request):
+    #project_context = {}
+    #for setting_name in CONTEXT_SETTINGS_VARIABLES:
+    #    project_context[setting_name] = getattr(settings, setting_name)
+
+    # Context variables pulled from django.conf.settings
+    project_context = {setting_name: getattr(settings, setting_name)
+                        for setting_name in CONTEXT_SETTINGS_VARIABLES }
+
+    return project_context
--- src/hipikat/styles.py
+++ src/hipikat/styles.py
@@ -1,7 +1,7 @@
 
 def context_processor(request):
-    context = {
+    style_context = {
         'site_css_hrefs': 'http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700|'
         'Roboto:400,400italic,500,700,700italic,300|Roboto+Slab:400,700',
     }
-    return context
+    return style_context
