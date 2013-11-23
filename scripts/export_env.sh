9a38628ab98ef626e919915e7f80e07b500ef831
Adam Wright <adam@hipikat.org>
2013-11-22 20:43:56 -0500

    Update .gitignore with Sass build files, lose export_env DJANGO_ prefix

--- scripts/export_env.sh
+++ scripts/export_env.sh
@@ -6,9 +6,5 @@ envfile_dir=$project_dir/var/env
 for file in $envfile_dir/*
 do
     file_name=${file##*/}
-    export DJANGO_$file_name=`cat $file`
+    export $file_name=`cat $file`
 done
-
-
-### Local settings
-export DJANGO_SETTINGS_CLASS=Development
