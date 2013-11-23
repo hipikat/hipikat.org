abdcede3cbc5c53ac0afca8884db4158d88d2f58
Adam Wright <adam@hipikat.org>
2013-11-23 19:45:20 +0800

    Fixes in make_secret_key.py

--- scripts/make_secret_key.py
+++ scripts/make_secret_key.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python
-import os
-import sys 
-from django.core.management import ManagementUtility
-
 import random
-from django.core.management.base import BaseCommand
-from django.conf import settings
 
 
 SECRET_KEY_LENGTH = 50
@@ -15,7 +9,7 @@ SECRET_KEY_CHARS = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
 def secret_key_string():
     return ''.join(
         [random.SystemRandom().choice(SECRET_KEY_CHARS)
-        for _ in range(SECRET_KEY_LENGTH)])
+         for _ in range(SECRET_KEY_LENGTH)])
 
 
 if __name__ == "__main__":
