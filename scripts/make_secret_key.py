be16c8f0c70005f5302b72416a2f7fbca61bb743
Adam Wright <adam@hipikat.org>
2013-11-23 06:25:20 -0500

    Fix scripts/make_secret_key.py

--- scripts/make_secret_key.py
+++ scripts/make_secret_key.py
@@ -3,6 +3,20 @@ import os
 import sys 
 from django.core.management import ManagementUtility
 
+import random
+from django.core.management.base import BaseCommand
+from django.conf import settings
+
+
+SECRET_KEY_LENGTH = 50
+SECRET_KEY_CHARS = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+
+
+def secret_key_string():
+    return ''.join(
+        [random.SystemRandom().choice(SECRET_KEY_CHARS)
+        for _ in range(SECRET_KEY_LENGTH)])
+
+
 if __name__ == "__main__":
-    os.environ["DJANGO_SETTINGS_MODULE"] = "revkom.settings.bootstrap"
-    ManagementUtility([__file__, 'make_secret_key']).execute()
+    print(secret_key_string()),
