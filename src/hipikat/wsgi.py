85a1ba59cecc74b2593ce2166838d5093a5d4990
Adam Wright <adam@hipikat.org>
2013-11-24 00:45:32 -0500

    Add working app/web server config files, remove elephantblog requirement
    - Add working uwsgi/nginx/etc. config files
    - Remove (trunk) feincms-elephantblog requirement
    - Add delicious link to vanity linkroll

--- src/hipikat/wsgi.py
+++ src/hipikat/wsgi.py
@@ -13,13 +13,18 @@ middleware here, or combine a Django application with an application of another
 framework.
 
 """
-import os
+#import os
 
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "hipikat.settings"
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hipikat.settings.default")
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hipikat.settings.default")
+
+# TODO: abstraction from where? uwsgi calls the virtualenv's local env
+# stuff but but... not postactivate... so project_settings not in path?
+import envdir
+envdir.read('/opt/hipikat.org-prod/var/env')
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
